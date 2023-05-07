414ac9fbbd10f698ddd4a77f104ad716188f648f
Pocoyo-hue <nicolashermozavillavicencio@gmail.com>
2023-05-07 15:28:54 -0500

    v2

--- static/script/script.js
+++ static/script/script.js
@@ -10,11 +10,64 @@ function delete_bin(id){
 }
 
 function list_bin(){
+    let y;
+    let o; 
     for(let i = 0; i < bin.length; i++){
+        y = bin[i].toString();
+        o = y;
+        y = y.charAt(0);
+        console.log(y);
+        o = o.charAt(1);
+        switch (y){
+            case "0":
+                y = "NOT";
+                break;
+            case "1":
+                y = "NAND";
+                break;
+            case "2":
+                y = "ADD";
+                break;
+            case "3":
+                y = "LDA";
+                break;
+            case "4":
+                y = "OUTA";
+                break;
+            case "5":
+                y = "OUTB";
+                break;
+            case "6":
+                y = "INA";
+                break;
+            case "7":
+                y = "RD";
+                break;
+            case "8":
+                y = "RA";
+                break;
+            case "9":
+                y = "LDRA";
+                break;
+            case "C":
+                y = "JPI";
+                break;
+            case "D":
+                y = "JPC";
+                break;
+            case "E":
+                y = "JPZ";
+                break;
+        }
         if(bin[i]!==null){
             document.write(`<table>
                     <tr>
-                        <td>${bin[i]}</td>
+                        <td>Nemónico</td>
+                        <td>Operando</td>
+                    </tr>
+                    <tr>
+                        <td>${y}</td>
+                        <td>${o}</td>
                     </tr>
                 </table>`);
         }
@@ -30,25 +83,68 @@ function hex_as_byte(){
     console.log(b);
 }
 
-function click_f(operando){
-    let not = document.getElementById('not');
-    let nand = document.getElementById('nand');
-    let add = document.getElementById('add');
-    let lda = document.getElementById('lda');
-    let outa = document.getElementById('outa');
-    let outb = document.getElementById('outb');
-    let ina = document.getElementById('ina');
-    let rd = document.getElementById('rd');
-    let ra = document.getElementById('ra');
-    let ldra = document.getElementById('ldra');
-    let jpi = document.getElementById('jpi');
-    let pjc = document.getElementById('jpc');
-    let jpz = document.getElementById('jpz');
+function click_f(f,operando){
+    let x;
+    switch (f){
+        case 0:
+            x = 0;
+            insert_bin(x);
+            break;
+        case 1:
+            x = 1+""+operando;
+            insert_bin(x);
+            break;
+        case 2:
+            x = 2+""+operando;
+            insert_bin(x);
+            break;
+        case 3:
+            x = 3+""+operando;
+            insert_bin(x);
+            break;
+        case 4:
+            x = 4;
+            insert_bin(x);
+            break;
+        case 5:
+            x = 5;
+            insert_bin(x);
+            break;
+        case 6:
+            x = 6;
+            insert_bin(x);
+            break;
+        case 7:
+            x = 7;
+            insert_bin(x);
+            break;
+        case 8:
+            x = 8;
+            insert_bin(x);
+            break;
+        case 9:
+            x = 9;
+            insert_bin(x);
+            break;
+        case 10:
+            x = "C"+""+operando;
+            insert_bin(x);
+            break;
+        case 11:
+            x = "D"+""+operando;
+            insert_bin(x);
+            break;
+        case 12:
+            x = "E"+""+operando;
+            insert_bin(x);
+            break;
+    }
 }
 
 function create_bin(){
     let name;
     let a = "";
+    hex_as_byte();
     for(let i = 0; i<b.length; i++){
         a=a+String.fromCharCode(b[i]);
     }
